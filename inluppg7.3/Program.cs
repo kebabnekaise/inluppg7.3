6bedcc0bf79a5690c0414e65a329c1a22916271f
kebabnekaise <emsi06001@utb.vaxjo.se>
2024-02-02 11:13:05 +0100

    Add project files.

---
+++ inluppg7.3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace inluppg7_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+            dictionary["sverige"] = 450;
+            dictionary["norge"] = 385;
+            dictionary["danmark"] = 43;
+            dictionary["finland"] = 338;
+
+            Console.WriteLine("Vilket nordiskt land vill du veta landarean på?");
+
+            string input = Console.ReadLine().ToLower();
+            Console.WriteLine(dictionary[input] + " m^2");
+        }
+    }
+}
